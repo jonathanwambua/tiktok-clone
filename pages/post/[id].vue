39cf9a7e479aef608d8a29ba903b6b73d64e7d70
jonathanwambua <jonathanwambua10219@gmail.com>
2023-10-05 17:02:07 +0300

    Initial upload

---
+++ pages/post/[id].vue
@@ -0,0 +1,18 @@
+<template>
+    <div
+        id="PostPage"
+        class="fixed lg:flex justify-between z-50 top-0 left-0 w-full bg-black"
+    >
+        <div class="lg:w-[calc(100%-540px)] h-full relative">
+            <NuxtLink
+                class="absolute z-20 m-5 rounded-full bg-gray-700 p-1.5 hover:bg-gray-800"
+                to="">
+                <Icon name="material-symbols:close" color="#ffffff" size="27" />
+            </NuxtLink>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
